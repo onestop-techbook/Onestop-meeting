e93f37260294b4268c9c826824778d5bf5c6c0a2
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2019-02-16 18:29:57 +0900

    IFTTTで拾えるようファイル名にタイムスタンプをつけるようにした

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 for TARGET in articles/*.pdf;
 do
   FILENAME=$(basename $TARGET)
+  FILENAME=$(basename $TARGET)
   curl -X POST -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
-	-D - -H "Dropbox-API-Arg: {\"path\": \"/${FILENAME}\",\"mode\": \"overwrite\",\"mute\": false}" \
+	-D - -H "Dropbox-API-Arg: {\"path\": \"/${FILENAME#.pdf}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
     -H "Content-Type: application/octet-stream" --data-binary @${TARGET} https://content.dropboxapi.com/2/files/upload
 done
